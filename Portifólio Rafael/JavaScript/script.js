0e3faa30c29ce04f0a02b7b147217af895f98a88
Rafael Carvalho <79648062+LeafCarvalho@users.noreply.github.com>
2022-02-22 12:38:08 -0300

    almost complete dark mode

--- Portifólio Rafael/JavaScript/script.js
+++ Portifólio Rafael/JavaScript/script.js
@@ -1,5 +1,15 @@
+//dark-mode
 const btn = document.getElementById("btn");
 
 btn.addEventListener("change", (e) => {
     document.body.classList.toggle("dark", e.target.checked);
 })
+
+
+//email
+document.getElementById('contact').addEventListener('submit', function () {
+    var nome = this.querySelector('input[name=nome]'), nome = nome.value;
+    var email = this.querySelector('input[name=email]'), email = email.value;
+    var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
+    this.querySelector('input[name=Body]').setAttribute('value', texto);
+});
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # meu-Portifolio
  Meu Portifólio :)
+
+Index.HTML:
+
+Utilizei o Bulma como bootstrap e montei meu currículo utilizando o HTML;
+
+Contato.html:
+
+Aqui está uma maneira de realizar contato comigo e que estou estudando uma maneira de criar um botão que envie um email direto para a minha caixa de correio.
+
+Style.css:
+
+Neste arquivo eu personalizei o site, fiz ele ficar mais bonito, mais estilizado e ainda trabalho em melhorias.
+
+Script.js:
+
+É onde faço objetos interativos funcionarem de acordo com o que for preciso.
